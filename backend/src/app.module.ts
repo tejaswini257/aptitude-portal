eac7fb189fad24f35947a8f474ecac0af168f397
Komal Deogade <komaldeogade07@gmail.com>
2026-01-12 11:52:29 +0530

    add

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,24 +7,6 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { DepartmentModule } from './department/department.module';
-<<<<<<< HEAD
-import { CollegesModule } from './modules/colleges/colleges.module';
-import { OrganizationController } from './organization/organization.controller';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
-    PrismaModule,
-    AuthModule,
-    DepartmentModule,
-    CollegesModule,   // ✅ IMPORTANT: THIS WAS MISSING
-  ],
-  controllers: [OrganizationController],
-  providers: [AppService],
-=======
 import { OrganizationModule } from './organization/organization.module';
 import { CollegeModule } from './college/college.module';
 
@@ -36,6 +18,5 @@ import { CollegeModule } from './college/college.module';
     OrganizationModule,  
     CollegeModule,
   ],
->>>>>>> ee26383073cc635599d22b82d0e83989f6464583
 })
 export class AppModule {}
