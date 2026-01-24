d3f7afef5ef3bb8bd56eeb37766d05303212eba5
Shreya20-ton <wallalwarshreya@gmail.com>
2026-01-24 15:01:09 +0530

    add

--- frontend/src/app/college/departments/page.tsx
+++ frontend/src/app/college/departments/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react"
 import Link from "next/link"
-import { api } from "@/lib/api"
+import api from "@/lib/api"
 import { useRouter } from "next/navigation"
 
 type Department = {
