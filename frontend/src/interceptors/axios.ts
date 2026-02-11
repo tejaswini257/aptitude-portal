3a0160d9b8ec2be9d007902b4b53e0594cb1b76f
Komal Deogade <komaldeogade07@gmail.com>
2026-02-11 13:31:10 +0530

    Updated axios for production

--- frontend/src/interceptors/axios.ts
+++ frontend/src/interceptors/axios.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  typeof window !== "undefined"
-    ? process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001"
-    : process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
-
 const api = axios.create({
-  baseURL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Cache-Control": "no-cache",
   },
@@ -15,10 +10,12 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
     const token = localStorage.getItem("accessToken");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
   }
+
   return config;
 });
 
