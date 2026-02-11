daae00042542f1a0a6b751800fe9e04692859819
Komal Deogade <komaldeogade07@gmail.com>
2026-02-11 14:55:57 +0530

    fix dynamic prerender issue

--- frontend/src/app/college/students/add/page.tsx
+++ frontend/src/app/college/students/add/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import { createStudent } from "@/services/student.service";
 import { useRouter } from "next/navigation";
